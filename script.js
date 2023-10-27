88c55dfc4c3984609fda1d9bb6b64481977df75f
Hüseyin Demirtaş <1660961+dilsayar@users.noreply.github.com>
2023-10-27 14:03:14 +0300

    theme and frame updates

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function drawFrame() {
 frameImage.onload = function() {
     drawFrame(); // Draw the frame once it's loaded
 }
-frameImage.src = '/images/t1.png';
+frameImage.src = '/images/t2.png';
 
 
 frameDropdown.addEventListener('change', function() {
